3dd7c9646d1e7f6bbc3c23a398033f816b9a56bd
MuhammadAsnan <asnandenim@gmail.com>
2017-02-25 19:33:43 +0700

    Create share.js

---
+++ share.js
@@ -0,0 +1,17 @@
+<b>Bagikan Artikel Ini Ke:</b>
+<br />
+<div class="fb-like" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+<div class="fb-share-button" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u&amp;src=sdkpreparse">Bagikan</a></div>
+<a href="https://twitter.com/share" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+<script type="text/javascript" src="https://apis.google.com/js/platform.js">
+  {lang: 'id'}
+</script>
+<div class="g-plus" data-action="share" data-annotation="bubble"></div>
+ <a href="http://www.pinterest.com/pin/create/button/
+        ?url=http%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkentbrew%2F6851755809%2F
+        &media=http%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg
+        &description=Next%20stop%3A%20Pinterest"
+        data-pin-do="buttonPin"
+        data-pin-config="above">
+        <img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" />
+    </a>
